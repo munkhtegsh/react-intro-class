03bf5a8915c742a99cb5b31be695c0dba4b05a93
Munkhtegsh Munkhbat <munkhtegsh@munkhtegshsimac.attlocal.net>
2017-12-11 23:16:25 -0800

    "Fixed the bug"

--- app/src/Component/Calculator/Calculator.js
+++ app/src/Component/Calculator/Calculator.js
@@ -3,11 +3,22 @@ import calculatorImg from '../../calculator.png';
 // import default from '../../App';
 
 class Calculator extends Component{
+    constructor() {
+        super();
+        this.state = {
+            header : "Calculator"
+        }
+    }
+    updateHeader(val) {
+        this.setState({header : val});
+    }
+
+
     render() {
         return (
             <div id="calculator-container">
-              <input id="header-input"/>
-              <h1 id="header"> Calculator </h1>
+              <input id="header-input" onChange={(e) => {this.updateHeader(e.target.value)}} />
+              <h1 id="header"> {this.state.header} </h1>
               <img className="remove-highlight" src={calculatorImg} alt="calculator" />
               <div id="calculator-mask" className="remove-highlight">
                 <div className="output">
