fd1cc7ec13909a7e9ff71c3fc3e7affc767c2e78
Munkhtegsh Munkhbat <munkhtegsh@munkhtegshsimac.attlocal.net>
2017-12-11 22:58:32 -0800

    The empty input will update the H1.

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Component/Calculator/Calculator.js';
+class App extends Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
+
+export default App;
